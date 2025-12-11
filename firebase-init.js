a6ca607f07eac8a420bc4cf80db2c29680bf45c4
shc25 <obil26@ischool.org.za>
2025-12-11 20:30:00 +0200

    Commented firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,5 @@
 // firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
